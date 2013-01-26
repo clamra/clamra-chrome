ebdce70f3e86756ec6d7d14b4ef025d95d59e13e
ftao <Filia.Tao@gmail.com>
2013-01-26 16:06:51 +0800

    select last one after new pobox

--- src/js/iframe.js
+++ src/js/iframe.js
@@ -1,34 +1,35 @@
 $(function(){
-      var poboxList = []
-      chrome.extension.sendRequest({method: "getOptions"}, function(options){
-          if(options.poboxList){
-            poboxList = options.poboxList.split(',');
-          }
-          fillEmailList();
-      });
+    var poboxList = []
+    chrome.extension.sendRequest({method: "getOptions"}, function(options){
+        if(options.poboxList){
+        poboxList = options.poboxList.split(',');
+        }
+        fillEmailList();
+    });
 
-      function fillEmailList(){
-          $("#emailList").empty();
-          $.each(poboxList, function(i, item){
-              $("#emailList").append("<option>" + item + "</option>");
-          });
-      }
-      $("#close").click(function(){
-          window.parent.postMessage({action:"closeIframe"},"*");
-      });
-      $("#btnOk").click(function(){
-           var email =  $("#emailList").val();
-          window.parent.postMessage({action:"setEmail",email:email},"*")
-          //messages.page.broadcast('iframeClosed', {email:email});
-          //messages.page.sendToTop('closeIframe');
-      });
+    function fillEmailList(){
+        $("#emailList").empty();
+        $.each(poboxList, function(i, item){
+            $("#emailList").append("<option>" + item + "</option>");
+        });
+    }
+    $("#close").click(function(){
+        window.parent.postMessage({action:"closeIframe"},"*");
+    });
+    $("#btnOk").click(function(){
+        var email =  $("#emailList").val();
+        window.parent.postMessage({action:"setEmail",email:email},"*")
+        //messages.page.broadcast('iframeClosed', {email:email});
+        //messages.page.sendToTop('closeIframe');
+    });
 
-      $("#btnNew").click(function(){
-          chrome.extension.sendRequest({method: "createPobox"}, function(poboxName){
-             poboxList.push(poboxName);
-             fillEmailList();
-          });
-      });
+    $("#btnNew").click(function(){
+        chrome.extension.sendRequest({method: "createPobox"}, function(poboxName){
+            poboxList.push(poboxName);
+            fillEmailList();
+            $("#emailList option:last").attr('selected', true);
+        });
+    });
 
 
 })
